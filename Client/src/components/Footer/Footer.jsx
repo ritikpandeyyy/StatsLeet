54e8c58c16cbe3b1c79cc5ddd8bc23aa64620ccf
Ritik Pandey <ritikpandey9536@gmail.com>
2025-11-16 15:10:37 +0530

    changes

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Heart } from "lucide-react";
 const Footer = () => {
   return (
     <div className="flex text-xs sm:text-base justify-center items-center m-2 space-x-1">
-      Developed with <Heart className="mx-1 w-4 sm:w-6" /> by Ritik Pandey
+      Developed with <Heart className="mx-1 w-4 sm:w-6" /> by Ritik Pandey..
     </div>
   );
 };
